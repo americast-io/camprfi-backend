ee353fe032bee7156583c0c918ae633024fb9ce9
Yurochkina <olga.yurochkina@gmail.com>
2023-03-26 16:21:48 -0500

    added allow origin header

--- server/utils/jwtToken.js
+++ server/utils/jwtToken.js
@@ -18,6 +18,7 @@ const sendToken = (user, statusCode, res) => {
     }
 
     // res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.header('Access-Control-Allow-Origin', 'http://3.83.146.55');
     res.header('Access-Control-Allow-Credentials', true);
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
 
