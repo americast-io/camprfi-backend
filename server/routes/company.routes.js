e72b4446aea44208f81d401ec6bb166e953cbee6
Yurochkina <olga.yurochkina@gmail.com>
2023-05-04 18:43:23 -0500

    Added company routes

---
+++ server/routes/company.routes.js
@@ -0,0 +1,23 @@
+const express = require('express');
+
+const {
+    handleCreateCompany,
+    handleGetAllCompanies,
+    handleGetCompanyById,
+    handleDeleteCompanyById,
+    handleUpdateCompanyById,
+
+} = require('../controllers/company.controller');
+
+const { isAuthenticatedUser, authorizeRoles } = require('../middlewares/auth');
+
+const router = express.Router();
+
+router.post('/', handleCreateCompany);
+router.get('/', handleGetAllCompanies);
+router.get('/:id', handleGetCompanyById);
+router.delete('/id', handleDeleteCompanyById);
+router.put('/id', handleUpdateCompanyById);
+
+module.exports = { companyRouter: router };
+
